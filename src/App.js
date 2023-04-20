eb73845648193ab338e34311d92cb762e53ad883
Gleb <pont.gleb@gmail.com>
2023-04-20 11:39:25 +0300

    On branch master Changes to be committed: 	deleted:    public/favicon.ico 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from 'react';
 
 function App() {
+  const [balance, setBalance] = useState(1000);
+  const [actions, setActions] = useState([]);
+  const [clientIndex, setClientIndex] = useState(1);
+  const [serverState, setServer] = useState('Сервер не запущен');
+  const [clientState, setClient] = useState(`Клиент${clientIndex} не запущен`);
+
+  const startServer = () => {
+    setServer('Сервер запущен');
+  };
+
+  const startClient = () => {
+    if (serverState==='Сервер не запущен') {
+      setClient('Для начала запустите сервер')
+      return;
+    }
+    setClientIndex(clientIndex+1);
+    setClient(`Клиент ${clientIndex} запущен`);
+    setInterval(() => changeBalance(clientIndex), 2000);
+  };
+
+  const changeBalance = (clientIndex) => {
+    let amount = Math.floor(Math.random() * 100) + 1;
+    amount *= Math.random() > 0.5 ? 1 : -1;
+    setBalance(balance + amount);
+    actions.push(`\n[Клиент${clientIndex}]Изменил баланс на ${amount}`);
+    setActions(actions);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={startServer}>Запустить сервер</button>
+      <button onClick={startClient}>Запустить клиент</button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div>Server: {serverState}</div>
+      <div>Client: {clientState}</div>
+      <div>Balance: {balance}</div>
+      {actions.map((action, divKey) => (
+        <div key={divKey}>{action}</div>
+      ))}
     </div>
   );
+  
 }
 
 export default App;
